c980ba6903510bc76a71296a149266bf36aa0f1d
AlyJ <alina.jarmolica2@gmail.com>
2020-09-02 19:01:04 +0300

    Initial Commit

---
+++ WeeklyDayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklyDayFinder
+//
+//  Created by alina.jarmolica on 02/09/2020.
+//  Copyright © 2020 alina.jarmolica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

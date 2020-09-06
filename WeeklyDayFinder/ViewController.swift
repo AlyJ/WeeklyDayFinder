329caaaaa60f710f2dc0b9b0274121b26dbb6189
AlyJ <alina.jarmolica2@gmail.com>
2020-09-06 15:58:47 +0300

    calendar

--- WeeklyDayFinder/ViewController.swift
+++ WeeklyDayFinder/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var dayTextFill: UITextField!
+    @IBOutlet weak var monthTextFill: UITextField!
+    @IBOutlet weak var yearTextFill: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    //this func makes the numer keyboead disapear when you click on any other space on the view
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+func handleCalculation(){
+    
+        let calendar = Calendar.current
+    
+        var dateComponent = DateComponents()
+    
+    guard let day = dayTextFill.text, let month = monthTextFill.text, let year = yearTextFill.text else {
+        return
+    }
+        dateComponent.day = Int(day)
+        dateComponent.month = Int(month)
+        dateComponent.year = Int(year)
+        
+    guard let date = calendar.date(from: dateComponent) else {
+        print("error")
+        return
+    }
+    
+         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "lv_LV")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday
+        
+        
+    }
+    
+    
+    @IBAction func findWeekdayTapped(_ sender: Any) {
+        handleCalculation()
+    }
+    
 }
 
